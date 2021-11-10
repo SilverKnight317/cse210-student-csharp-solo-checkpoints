40737c97930f9f8ffc2e4deaaea90f89750a2c3e
SilverKnight317 <ajclarkson3@gmail.com>
2021-11-10 11:46:28 -0700

    Added DrawActorsAction lines to operate

--- 08-rfk/Scripting/DrawActorsAction.cs
+++ 08-rfk/Scripting/DrawActorsAction.cs
@@ -7,4 +7,23 @@ namespace _08_rfk.Scripting
 {
     // TODO: Add your class here to draw all the actors in the game
     // making use of the OutputService object.
+    class DrawActorsAction : Action
+    {
+        OutputService _outputService;
+        public DrawActorsAction(OutputService outputService)
+        {
+            _outputService = outputService;
+        }
+        public override void Execute(Dictionary<string, List<Actor>> cast)
+        {
+            // open one and then close it once
+            _outputService.StartDrawing();
+            // base.Execute(cast);
+            foreach(List<Actor> artifact in cast.Values)
+            {
+                _outputService.DrawActors(artifact);
+            }
+            _outputService.EndDrawing();
+        }
+    }
 }
